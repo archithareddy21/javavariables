c4c865f48e579592dbb37b3461c179994e50f168
archithareddy21 <92724982+archithareddy21@users.noreply.github.com>
2022-02-07 12:09:07 +0530

    variables are committed

---
+++ 07-02-2022/src/com/javaVariables/Example1.java
@@ -0,0 +1,35 @@
+package com.javaVariables;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		//write a java program using java variables and literals 
+		
+		//variable declaration
+		int StudentId;
+		String StudentName;
+		float StudentMarks;
+		String StudentAddress;
+		//variable initialization
+		StudentId=3020;
+		StudentName="Akshara";
+		StudentMarks=64.55f;
+		StudentAddress="Pune";
+		 
+		System.out.print("StudentId:");
+		System.out.println(StudentId);
+		System.out.print("StudentName:");
+		System.out.println(StudentName);
+		System.out.print("StudentMarks:");
+		System.out.println(StudentMarks);
+		System.out.print("StudentAddress:");
+		System.out.println(StudentAddress);
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ 07-02-2022/src/com/javaVariables/Example2.java
@@ -0,0 +1,55 @@
+package com.javaVariables;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		// variable declaration
+		int studentId;
+		String studentName;
+		int studentAge;
+		float studentMarks;
+		String studentGrade;
+		String studentResult;
+		String studentAddress;
+		
+		//variable initialization
+		studentId=2222;
+		studentName="lia";
+		studentAge=22;
+		studentMarks=88.4f;
+		studentGrade="A";
+		studentResult="pass";
+		studentAddress="Delhi";
+		
+		System.out.print("studentId :");
+		System.out.println(studentId);
+		System.out.print("studentName :");
+		System.out.println(studentName);
+		System.out.print("studentage :");
+		System.out.println(studentAge);
+		System.out.print("studentMarks :");
+		System.out.println(studentMarks);
+		System.out.print("studentgrade :");
+		System.out.println(studentGrade);
+		System.out.print("studentresult :");
+		System.out.println(studentResult);
+		System.out.print("studentAddress :");
+		System.out.println(studentAddress);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
